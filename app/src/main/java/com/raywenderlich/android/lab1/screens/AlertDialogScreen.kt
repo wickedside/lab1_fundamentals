cb8943108436c414f56f3e3662a19b85fef496c0
wickedside <w1ck3ds1d3@gmail.com>
2023-02-21 00:05:44 +0300

    added MyAlertDialog function

--- app/src/main/java/com/raywenderlich/android/lab1/screens/AlertDialogScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/AlertDialogScreen.kt
@@ -1,6 +1,15 @@
 package com.raywenderlich.android.lab1.screens
 
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import com.raywenderlich.android.lab1.R
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
 import com.raywenderlich.android.lab1.router.Screen
@@ -17,5 +26,31 @@ fun AlertDialogScreen() {
 
 @Composable
 fun MyAlertDialog() {
-    //TODO add your code here
+    val shouldShowDialog = remember { mutableStateOf(true) } // 1
+
+    if (shouldShowDialog.value) { // 2
+        AlertDialog( // 3
+            onDismissRequest = {
+                shouldShowDialog.value = false
+                FundamentalsRouter.navigateTo(Screen.Navigation)
+            },
+            // 5
+            title = { Text(text = stringResource(id = R.string.title_alertDialog)) },
+            text = { Text(text = stringResource(id =  R.string.alertDialog_about)) },
+            confirmButton = { // 6
+                Button(
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.Blue),
+                    onClick = {
+                        shouldShowDialog.value = false
+                        FundamentalsRouter.navigateTo(Screen.Navigation)
+                    }
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.massage_button),
+                        color = Color.White
+                    )
+                }
+            }
+        )
+    }
 }
