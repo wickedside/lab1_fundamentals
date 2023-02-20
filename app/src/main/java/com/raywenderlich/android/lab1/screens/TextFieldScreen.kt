b54acd851adf74db7da19bc521589e3112b61d5a
wickedside <w1ck3ds1d3@gmail.com>
2023-02-20 23:04:10 +0300

    edited MyTextField function

--- app/src/main/java/com/raywenderlich/android/lab1/screens/TextFieldScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/TextFieldScreen.kt
@@ -3,7 +3,11 @@ package com.raywenderlich.android.lab1.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.raywenderlich.android.lab1.router.BackButtonHandler
@@ -27,5 +31,11 @@ fun TextFieldScreen() {
 
 @Composable
 fun MyTextField() {
-    //TODO add your code here
+    val textValue = remember { mutableStateOf("")}
+
+    TextField(
+        value = textValue.value,
+        onValueChange = {textValue.value = it},
+        label = {}
+    )
 }
